1e699fe6011576d7ca82c02a2a386fe533c16816
Edmund Troche <edmund@troche.org>
2018-02-21 22:47:39 -0600

    Remove commented code.

--- src/main/java/org/troche/spring/application/exercise/model/WordFrequencyAnalysisSerializer.java
+++ src/main/java/org/troche/spring/application/exercise/model/WordFrequencyAnalysisSerializer.java
@@ -2,7 +2,6 @@ package org.troche.spring.application.exercise.model;
 
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonProcessingException;
-//import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 
